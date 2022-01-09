396ad28bf7193911c1f112aa7f6bd8c408ea25e8
Rodrigo Cunha <github@docunha.com.br>
2022-01-09 17:01:16 -0300

    Fixed Mongoose credentials

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://api:HmXRvIHE3FUGNXXK@shorturl.fenid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		'mongodb+srv://<user>:<password>@shorturl.fenid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
